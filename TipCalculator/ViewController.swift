1199f94487f99a709a0c0b85c6fb993f271cf9ce
snamiki1212 <snamiki1212@gmail.com>
2021-05-07 18:30:13 -0700

    Add skelton

--- TipCalculator/ViewController.swift
+++ TipCalculator/ViewController.swift
@@ -11,9 +11,25 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    @IBOutlet var tipAmountLabel: UILabel!
+    
+    @IBOutlet var billAmountTextField: UITextField!
+    
+    let tipRate: Double = 15
+    
+    @IBAction func calculateTipTapped(_ sender: UIButton) {
+        guard billAmountTextField.hasText else { return }
+        let text: String = billAmountTextField.text!
+        if let num = Int(text) {
+            let amount = Double(num) * ( 1.0 + tipRate / 100.0 )
+            tipAmountLabel.text = String(Int(amount))
+        }
+    }
+    
+    
+   
+    
 }
 
